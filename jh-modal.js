b4e7c8dac74ad01b404d5627f383b87d37845a6d
Nicholas Lash <njlash@gmail.com>
2015-12-11 09:57:53 -0600

    Added toolbar with new example, and more comments.
    Added toolbar with Quasi-ExtJS syntax. Add custom handlers for toolbar
    buttons that can reference the outer Modal container.

--- jh-modal.js
+++ jh-modal.js
@@ -1,6 +1,12 @@
-// Create an immediately invoked functional expression to wrap our code
 (function() {
-
+    // constructor for the bottom bar
+    this.BarButton = function() {
+        var defaults = { 
+            buttonText:  'Sample Text',
+            className: '',
+            handler: undefined
+        }
+    }
     // Define our constructor
     this.Modal = function() {
 
@@ -20,7 +26,8 @@
             content: "",
             maxWidth: 600,
             minWidth: 280,
-            overlay: true
+            overlay: true,
+            toolbarButtons: [],
         }
 
         // Create options by extending defaults with the passed in arugments
@@ -39,18 +46,23 @@
         this.modal.className = this.modal.className.replace(" jh-open", "");
         this.overlay.className = this.overlay.className.replace(" jh-open",
         "");
+        
         this.modal.addEventListener(this.transitionEnd, function() {
             _.modal.parentNode.removeChild(_.modal);
         });
         this.overlay.addEventListener(this.transitionEnd, function() {
             if(_.overlay.parentNode) _.overlay.parentNode.removeChild(_.overlay);
         });
-    }
-
+    },
+    
     Modal.prototype.open = function() {
         buildOut.call(this);
         initializeEvents.call(this);
         window.getComputedStyle(this.modal).height;
+
+        this.modal.className = this.modal.className.replace(" invisible", "");
+        this.overlay.className = this.overlay.className.replace(" invisible", "",
+            "");
         this.modal.className = this.modal.className +
         (this.modal.offsetHeight > window.innerHeight ?
             " jh-open jh-anchored" : " jh-open");
@@ -59,13 +71,55 @@
 
     // Private Methods
 
+    
+    function buttonBarInit() {
+        var buttonBar = '<div class="toolbar">';
+        var buttonBarItems = this.options.toolbarButtons;
+        var counter = 0;
+        buttonBarItems.map(function(item) {
+            buttonBar += '<button type="button" id="b_' + counter + '" class="' + item.className +  '">' + item.buttonText + '</button>&nbsp;&nbsp;';     
+            counter++;
+        });
+        button = '</div>'
+        return buttonBar;
+    }
+     
+    function buttonBarAddHandlers() {
+        /* 
+        * Once the DOM element of the popup is created, call to add event handlers.
+        */
+
+        var buttons = this.options.toolbarButtons;
+        var len = buttons.length;
+        for (var i = 0; i < len; i++) {
+            var button = document.getElementById('b_' + i);
+            buttonHandler = buttons[i].handler;
+            var self = this;
+            if (buttonHandler !== undefined) {
+                attachEvent.call(this, button, 'click', buttonHandler);
+            }
+        }
+    }
+    
+    function attachEvent(element, event, handler)
+    {
+        /*
+        * Helper method to mitigate scope hell.
+        */
+        if (element && element.attachEvent) {
+            element.attachEvent("on" + event, handler);
+        }
+        else if (element && element.addEventListener) {
+            element.addEventListener(event, handler, false);
+        }
+    }
+    
     function buildOut() {
-
-        var content, contentHolder, docFrag;
-
+        var content, toolbar, contentHolder, docFrag;
         /*
         * If content is an HTML string, append the HTML string.
         * If content is a domNode, append its content.
+        * If toolbarButtons is non-empty, add the buttons.
         */
 
         if (typeof this.options.content === "string") {
@@ -73,7 +127,12 @@
         } else {
             content = this.options.content.innerHTML;
         }
-
+        
+        // add buttons
+        if (this.options.toolbarButtons.length > 0) {
+            toolbar = buttonBarInit.call(this);
+        }
+        
         // Create a DocumentFragment to build with
         docFrag = document.createDocumentFragment();
 
@@ -101,7 +160,7 @@
         // Create content area and append to modal
         contentHolder = document.createElement("div");
         contentHolder.className = "jh-content";
-        contentHolder.innerHTML = content;
+        contentHolder.innerHTML = content + toolbar;
         this.modal.appendChild(contentHolder);
 
         // Append modal to DocumentFragment
@@ -109,7 +168,11 @@
 
         // Append DocumentFragment to body
         document.body.appendChild(docFrag);
-
+        
+        // Add event handlers to the buttons
+        if (this.options.toolbarButtons.length > 0) {
+            buttonBarAddHandlers.call(this);        
+        }
     }
 
     function extendDefaults(source, properties) {
@@ -123,7 +186,9 @@
     }
 
     function initializeEvents() {
-
+        /*
+        * Add events that close the window.
+        */
         if (this.closeButton) {
             this.closeButton.addEventListener('click', this.close.bind(this));
         }
@@ -131,7 +196,6 @@
         if (this.overlay) {
             this.overlay.addEventListener('click', this.close.bind(this));
         }
-
     }
 
     function transitionSelect() {
@@ -141,7 +205,8 @@
         return 'transitionend';
     }
 
-}());
+})();
+
 
 // Demo modal 1 - uncomment below
 // var myContent = document.getElementById('content');
@@ -184,4 +249,32 @@
 //
 // triggerButton.addEventListener('click', function() {
 //   myModal.open();
+
+
+//Demo Modal 4: Demo with button bar - uncomment below
+// var triggerButton = document.getElementById('trigger');
+// myModal = new Modal({
+//     content: '<h1>Loading&#8230;</h1><p>This is great!</p>',
+//     className: 'zoom',
+//     closeButton: true,
+//     toolbarButtons: [{
+//         buttonText: 'Show an alert and close.',
+//         className: '',
+//         handler: function() {
+//             myModal.close();
+//             alert('button handler');
+//         }
+//     }, {
+//         buttonText: 'Just get me outta here!',
+//         className: '',
+//         handler: function() {
+//             myModal.close();
+//         }
+//     }]
+// });
+//
+// var triggerButton = document.getElementById('trigger');
+//
+// triggerButton.addEventListener('click', function() {
+//     myModal.open();
 // });
